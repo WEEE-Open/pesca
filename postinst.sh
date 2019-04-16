088b512e12333ded33fc2309e43ebbfa766e8d65
lvps <lvps@users.noreply.github.com>
2019-04-16 23:33:07 +0200

    Typo and set -e

--- postinst.sh
+++ postinst.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Se un comando fallisce, lo script termina (visto che c'è roba che va in /dev/null ha senso)
+set -e
+
 # Questo è lo script da eseguire dopo l'installazione di Xubuntu sui PC da donare
-echo "Attivo aggiornateo orario tramite NTP"
+echo "Attivo aggiornamento orario tramite NTP"
 sudo timedatectl set-ntp true > /dev/null
 echo "Aggiorno repository"
 sudo apt update > /dev/null
