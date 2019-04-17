9f205a04599607f440a95c1f8586a1d788e1b6c4
Andrea Mannarella <andrimanna@users.noreply.github.com>
2019-04-17 21:13:08 +0200

    aggiuntte features fighissime

--- pesca.sh
+++ pesca.sh
@@ -17,42 +17,53 @@ installa () {
 wget -q --tries=10 --timeout=20 --spider http://weeeopen.polito.it/
 while [[ $? -ne 0 ]]
 do
-  echo "Attacca il cavo, WEEEino caro!"
+  echo -e "Attacca il cavo, \e[42;97mWEEEino\e[0m caro!"
   sleep 2
   wget -q --tries=10 --timeout=20 --spider http://weeeopen.polito.it/
 done
-echo "Attivo aggiornamento orario tramite NTP."
-sudo timedatectl set-ntp true
-echo "Fatto. Ora aggiorno e installo software nuovo di zecca."
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install vlc oneko
-#installa
-echo "Ho finito. Ora è possibile spegnere questo rottame e sbatterlo nella GroundZone."
-if [[ $1 = "-sn" ]]
+echo "Benvenuto nella 🍑!"
+echo -e "\e[1;34mAttivo aggiornamento orario tramite NTP..."
+if sudo timedatectl set-ntp true
 then
-  echo "Spengo il PC."
-  # shutdown now
-  exit 0
-fi
-read -p "Hai aggiornato il tarallo? VERO? [s/n] " response
-response=${response,,}
-if [[ "$response" =~ ^(si|s)$ ]]
-then
-  echo "Ma che bravo!"
+  echo -e "\e[1;92mFatto\e[0m. \e[1;34mAggiorno e installo software nuovo di zecca..."
+  if sudo apt update && sudo apt -y upgrade && sudo apt -y install vlc oneko
+  #installa
+  then
+    clear && echo -e "\e[1;92mHo finito.\e[1m Ora è possibile spegnere questo rottame e sbatterlo nella \e[102;97mG\e[107;92mr\e[102;97mo\e[107;92mu\e[102;97mn\e[107;92md\e[102;97mZ\e[107;92mo\e[102;97mn\e[107;92me\e[0m."
+    if [[ $1 = "-sn" ]]
+    then
+      echo "Spengo il PC."
+      # shutdown now
+      exit 0
+    fi
+    echo -e "Hai aggiornato il tarallo? \e[5;1mVERO\e[0m? [s/n]"
+    read response
+    response=${response,,}
+    if [[ "$response" =~ ^(si|s)$ ]]
+    then
+      echo "Ma che bravo!"
+    else
+      echo "Che cosa stai aspettando: fallo subito! https://tarallo.weeeopen.it/"
+    fi
+    echo "Ora vuoi spegnere il PC? [s/n]"
+    read responsea
+    responsea=${responsea,,}
+    if [[ "$responsea" =~ ^(si|s)$ ]]
+    then
+      echo "Va bene: tra 10 secondi il PC si spegnerà. Nel frattempo gioca pure con questo gattino."
+      timeout 10 oneko
+      echo "Ciao."
+    # shutdown now
+      exit 0
+    else
+      echo "Ok, per te niente gattino. Ciao."
+      exit 0
+    fi
+  else
+    echo -e "\e[91;1mSi è verificato un errore in apt. Controlla la console."
+    exit 1
+  fi
 else
-  echo "Che cosa stai aspettando: fallo subito! https://tarallo.weeeopen.it/"
-fi
-read -p "Ora vuoi spegnere il PC? [s/n] " responsea
-responsea=${responsea,,}
-if [[ "$responsea" =~ ^(si|s)$ ]]
-then
-  echo "Va bene: tra 10 secondi il PC si spegnerà. Nel frattempo gioca pure con questo gattino."
-  timeout 10 oneko
-  echo "Ciao."
-# shutdown now
-  exit 0
-else
-  echo "Ok, per te niente gattino. Ciao"
-  exit 0
+  echo -e "\e[91;1mSi è verificato un errore in timedatectl. Controlla la console."
+  exit 1
 fi
