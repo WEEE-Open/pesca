ee3887fa76422662473cf6b6a59db8af1909362f
Andrea Mannarella <andrimanna@users.noreply.github.com>
2020-02-27 20:47:56 +0100

    cosette carine

--- pesca.sh
+++ pesca.sh
@@ -2,40 +2,44 @@
 
 # Questo è lo script da eseguire dopo l'installazione di Xubuntu sui PC da donare
 
-sudo echo -e "\e[0;1m"
+sudo echo -en "\e[0;1m"
 while ! wget -q --tries=10 --timeout=20 --spider http://weeeopen.polito.it/
 do
   echo -e "Attacca il cavo, \e[42;97mWEEEino caro!\e[0;1m"
-  sleep 2
+  sleep 3
 done
 echo -e "Benvenuto nella 🍑!"
-echo -e "\e[31mAttivo aggiornamento orario tramite NTP...\e[0;1m"
+echo -en "\e[31mAttivo aggiornamento orario tramite NTP...\e[0;1m"
+echo NTP=ntp1.inrim.it ntp2.inrim.it | sudo tee -a /etc/systemd/timesyncd.conf > /dev/null
 sudo timedatectl set-ntp true
-echo -e "\e[32mInstallo il tema Plymouth...\e[0;1m"
+echo -e "\t\t\t\t\e[92m✔️"
+echo -en "\e[32mInstallo motd e il tema Plymouth...\e[0;1m"
 sudo cp -r weee-logo/ /usr/share/plymouth/themes/weee-logo
 sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/weee-logo/weee-logo.plymouth 100
-sudo update-alternatives --set default.plymouth /usr/share/plymouth/themes/weee-logo/weee-logo.plymouth
+sudo xterm -geometry 80x24-0-0 -e update-alternatives --set default.plymouth /usr/share/plymouth/themes/weee-logo/weee-logo.plymouth
 sudo cp 01-weeeopen /etc/update-motd.d/01-weeeopen
 sudo chown root:root /etc/update-motd.d/01-weeeopen
 sudo chmod 755 /etc/update-motd.d/01-weeeopen
-echo -e "\e[33mAggiorno e installo software nuovo, fiammante..."
+echo -e "\t\t\t\t\t\e[92m✔️"
+echo -en "\e[33mMentre aggiorno il software, gioca pure con questo gattino...\e[0;1m"
 sudo xterm -geometry 80x24-0-0 -e apt update
 sudo xterm -geometry 80x24-0-0 -e apt install oneko -y
-echo -e "\e[34mNel frattempo gioca pure con questo gattino."
 oneko &
 sudo xterm -geometry 80x24-0-0 -e apt upgrade -y
 sudo xterm -geometry 80x24-0-0 -e apt install vlc -y
-echo -e "\e[35mFatto.\e[0;1m Ora è possibile spegnere questo rottame e sbatterlo nella \e[102;93mG\e[103;92mr\e[102;93mo\e[103;92mu\e[102;93mn\e[103;92md\e[102;93mZ\e[103;92mo\e[102;93mn\e[103;92me\e[0;1m."
-echo -e "\e[36mSpero tu abbia aggiornato il tarallo. \e[5mVERO?\e[0;1m"
-echo -e "\e[91mPulisco i pacchetti superflui...\e[0;1m"
+echo -e "\t\t\e[92m✔️"
+echo -en "\e[94mPulisco i pacchetti superflui...\e[0;1m"
 killall oneko
 sudo xterm -geometry 80x24-0-0 -e apt autoremove oneko language-pack-en -y
+echo -e "\t\t\t\t\t\e[92m✔️"
+echo -e "\e[0;1mOra è possibile spegnere questo rottame e sbatterlo nella \e[0;102;30mG\e[103mr\e[102mo\e[103mu\e[102mn\e[103md\e[102mZ\e[103mo\e[102mn\e[103me\e[0;1m."
+echo -e "\e[36mSpero tu abbia aggiornato il tarallo. \e[5mVERO?\e[0;1m"
 if [[ $1 = "-sn" ]]
 then
   shutdown now
   exit 2
 fi
-echo -e "Ora vuoi spegnere il PC?\e[0m [s/N]"
+echo -en "Ora vuoi spegnere il PC? \e[0m [s/N]"
 read response
 response=${response,,}
 if [[ "$response" =~ ^(si|s)$ ]]
